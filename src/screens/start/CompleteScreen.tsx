960960acccde266504d10140bd89c63c213d1040
Yoonjae Yoo <ryunzae@gmail.com>
2019-06-21 11:19:56 +0900

    Close #22

--- src/screens/start/CompleteScreen.tsx
+++ src/screens/start/CompleteScreen.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { Image, View } from "react-native";
 
 import { Updates } from "expo";
-import { Button, Container, Text } from "native-base";
+import { Button, Container, Content, Text } from "native-base";
 import CaptionText from "../../components/CaptionText";
 import SubtitleText from "../../components/SubtitleText";
 import { Spacing } from "../../constants/dimension";
@@ -15,28 +15,30 @@ const CompleteScreen = () => {
     }, []);
     return (
         <Container style={{ flex: 1 }}>
-            <SubtitleText aboveText={true}>{t("start:complete")}</SubtitleText>
-            <CaptionText>{t("start:complete.description")}</CaptionText>
-            <View style={{ flex: 1, margin: Spacing.normal }}>
-                <Image
-                    source={require("../../assets/rabbit.jpg")}
-                    fadeDuration={0}
-                    style={{
-                        width: 237,
-                        height: 333,
-                        margin: Spacing.large,
-                        alignSelf: "center"
-                    }}
-                />
-                <Button
-                    info={true}
-                    block={true}
-                    large={true}
-                    style={{ marginTop: Spacing.normal }}
-                    onPress={onComplete}>
-                    <Text>{t("start")}</Text>
-                </Button>
-            </View>
+            <Content>
+                <SubtitleText aboveText={true}>{t("start:complete")}</SubtitleText>
+                <CaptionText>{t("start:complete.description")}</CaptionText>
+                <View style={{ flex: 1, margin: Spacing.normal }}>
+                    <Image
+                        source={require("../../assets/rabbit.jpg")}
+                        fadeDuration={0}
+                        style={{
+                            width: 237,
+                            height: 333,
+                            margin: Spacing.large,
+                            alignSelf: "center"
+                        }}
+                    />
+                    <Button
+                        info={true}
+                        block={true}
+                        large={true}
+                        style={{ marginTop: Spacing.normal }}
+                        onPress={onComplete}>
+                        <Text>{t("start")}</Text>
+                    </Button>
+                </View>
+            </Content>
         </Container>
     );
 };
