7dad26c71af43eeb7a80e9787ff743051dd56766
Yoonjae Yoo <ryunzae@gmail.com>
2019-07-11 06:32:02 +0900

    ¡Español!

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,9 +3,10 @@ import { initReactI18next } from "react-i18next";
 import * as Localization from "expo-localization";
 import i18n from "i18next";
 import en from "./locales/en.json";
+import es from "./locales/es.json";
 import ko from "./locales/ko.json";
 
-const resources = { en, ko };
+const resources = { en, es, ko };
 
 const languageDetector = {
     type: "languageDetector",
@@ -24,7 +25,7 @@ i18n.use(initReactI18next)
     .init({
         resources,
         lng: Localization.locale,
-        whitelist: ["en", "ko"],
+        whitelist: ["en", "es", "ko"],
         fallbackLng: "en",
         keySeparator: false, // we do not use keys in form messages.welcome
         interpolation: {
