88ba67e0f8062436a74284f286866e276fa0c8a5
Changhwan Choi <changhwan@dnext.co>
2019-07-17 12:22:24 +0900

    Fix crash when "en-US" locale is setted
    - Filter locale string

--- src/components/AgoText.tsx
+++ src/components/AgoText.tsx
@@ -5,9 +5,23 @@ import * as Localization from "expo-localization";
 import "moment/min/locales";
 import { Text } from "native-base";
 
+const filterLocale = (locale: string): string => {
+    locale = locale.toLowerCase();
+
+    if (locale === "en-us") {
+        return "en";
+    }
+
+    if (locale.indexOf("-") > 0) {
+        return locale.substring(0, locale.indexOf("-"));
+    }
+
+    return "en";
+};
+
 const AgoText = ({ date }: { date: Date }) => {
-    const [locale, setLocale] = useState<string>();
-    Localization.getLocalizationAsync().then(({ locale: l }) => setLocale(l));
+    const [locale, setLocale] = useState<string>(() => filterLocale(Localization.locale));
+    Localization.getLocalizationAsync().then(({ locale: l }) => setLocale(filterLocale(l)));
     return (
         <Moment
             locale={locale}
