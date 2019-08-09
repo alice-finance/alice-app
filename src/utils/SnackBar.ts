2a748537b1095659ff94ab27d2be44659d21304a
Changhwan Choi <changhwan@alice.finance>
2019-08-09 14:07:01 +0900

    Add bottom padding to SnackBar on iPhone X or above

--- src/utils/SnackBar.ts
+++ src/utils/SnackBar.ts
@@ -1,9 +1,38 @@
 import NativeSnackBar from "rn-snackbar";
 import platform from "../../native-base-theme/variables/platform";
+import { Dimensions, Platform } from "react-native";
+
+// See https://mydevice.io/devices/ for device dimensions
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
+
+const getResolvedDimensions = () => {
+    const { width, height } = Dimensions.get("window");
+    if (width === 0 && height === 0) {
+        return Dimensions.get("screen");
+    }
+    return { width, height };
+};
+
+const { height: D_HEIGHT, width: D_WIDTH } = getResolvedDimensions();
+
+const isIPhoneX = (() => {
+    return (
+        (Platform.OS === "ios" &&
+            ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) || (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT))) ||
+        ((D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) ||
+            (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT))
+    );
+})();
+
+const paddingBottom = isIPhoneX ? 34 : 0;
 
 export default class SnackBar {
     public static success = (text: string) => {
         NativeSnackBar.show(text, {
+            style: { paddingBottom },
             backgroundColor: platform.brandSuccess,
             textColor: platform.brandLight
         });
@@ -11,6 +40,7 @@ export default class SnackBar {
 
     public static danger = (text: string) => {
         NativeSnackBar.show(text, {
+            style: { paddingBottom },
             backgroundColor: platform.brandDanger,
             textColor: platform.brandLight
         });
